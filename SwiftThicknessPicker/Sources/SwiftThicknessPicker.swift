d4aca951a25eaf89b9963c32dae3696aada3ec1b
Maxim Bilan <maximb.mail@gmail.com>
2015-05-12 10:46:03 +0300

    Modified SwiftThinknessPicker

--- SwiftThicknessPicker/Sources/SwiftThicknessPicker.swift
+++ SwiftThicknessPicker/Sources/SwiftThicknessPicker.swift
@@ -8,14 +8,62 @@
 
 import UIKit
 
+protocol SwiftThicknessPickerDelegate {
+	func valueChanged(value: Int)
+}
+
 class SwiftThicknessPicker: UIView {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+	// Direction
+	
+	enum PickerDirection: Int {
+		case Horizontal
+		case Vertical
+	}
+	
+	// Private properties
+	
+	var delegate: SwiftThicknessPickerDelegate!
+	var direction: PickerDirection = .Horizontal
+	
+	// Initialization
+	
+	required init(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		self.backgroundColor = UIColor.clearColor()
+	}
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		self.backgroundColor = UIColor.clearColor()
+	}
+	
+	override func layoutSubviews() {
+		super.layoutSubviews()
+	}
+	
+	// Drawing
+	
+	override func drawRect(rect: CGRect) {
+		super.drawRect(rect)
+		
+		let radius = (direction == .Horizontal ? self.frame.size.height : self.frame.size.width)
+		let circleRect = (direction == .Horizontal ? CGRectMake(0, 0, radius, radius) : CGRectMake(0, 0, radius, radius))
+		
+		let context = UIGraphicsGetCurrentContext();
+		UIColor.yellowColor().set()
+		//CGContextAddEllipseInRect(context, circleRect);
+		
+		CGContextBeginPath(context);
+		CGContextMoveToPoint   (context, CGRectGetMaxX(rect), CGRectGetMinY(rect));  // top left
+		CGContextAddLineToPoint(context, CGRectGetMinX(rect), CGRectGetMidY(rect));  // mid right
+		CGContextAddLineToPoint(context, CGRectGetMaxX(rect), CGRectGetMaxY(rect));  // bottom left
+		CGContextClosePath(context);
+		
+		CGContextSetFillColor(context, CGColorGetComponents(UIColor.yellowColor().CGColor));
+		CGContextFillPath(context);
+		CGContextStrokePath(context);
+		
+	}
+	
 }
