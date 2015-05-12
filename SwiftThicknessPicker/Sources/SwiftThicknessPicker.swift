c122c4110d356fbf2ef3c808ae2ea1bbed1b4169
Maxim Bilan <maximb.mail@gmail.com>
2015-05-12 10:31:52 +0300

    Added SwiftThicknessPicker class

---
+++ SwiftThicknessPicker/Sources/SwiftThicknessPicker.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftThicknessPicker.swift
+//  SwiftThicknessPicker
+//
+//  Created by Maxim Bilan on 5/12/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+import UIKit
+
+class SwiftThicknessPicker: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
